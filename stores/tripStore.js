a0349f543f274e9d92bbb3c4e90f230a28599c6a
Your Name <mohammadalhamdan94@gmail.com>
2022-02-23 10:16:01 +0300

    mnm2

--- stores/tripStore.js
+++ stores/tripStore.js
@@ -18,6 +18,32 @@ class TripStore {
       console.log("error In FetchTrips");
     }
   };
+
+  removeTrips = async (tripId) => {
+    try {
+      await api.delete(`/trips/${tripId}`);
+      this.trips = this.trips.filter((trip) => trip._id !== tripId);
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: tripStore.js ~ line 26 ~ TripStore ~ removeTrips= ~ error",
+        error
+      );
+    }
+  };
+  updateTrips = async (tripId) => {
+    try {
+      await api.put(`/trips/${updatedTrips._id}`);
+      let tripsUpdate = this.trips.map((trip) =>
+        trip._id === tripsUpdate._id ? response.data : trip
+      );
+      this.trips = tripsUpdate;
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: tripStore.js ~ line 37 ~ TripStore ~ updateTrips= ~ error",
+        error
+      );
+    }
+  };
 }
 const tripStore = new TripStore();
 tripStore.fetchTrips();
